e41a978769dc9195462274c41e666e25585939aa
rafatgomaa <rafatgomaa815@gmail.com>
2025-05-11 12:29:22 +0300

    Adding user controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_11_091829) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_11_092657) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "content"
@@ -25,5 +25,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_11_091829) do
     t.string "phone_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 end
