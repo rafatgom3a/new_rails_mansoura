962d3e6650c2a556430010c1fa9bbb8f6f8e3e10
rafatgomaa <rafatgomaa815@gmail.com>
2025-05-11 12:22:22 +0300

    Generate user model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_11_074512) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_11_091829) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.date "dob"
+    t.string "email"
+    t.string "phone_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
