93f880b3e57c6054953ee55adc6d16d1b8f97fb4
rafatgomaa <rafatgomaa815@gmail.com>
2025-05-11 12:15:07 +0300

    Adding posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,41 @@
 class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params.require(:post).permit(:title, :content))
+    if @post.save
+      redirect_to @post, notice: "Post was successfully created."
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(params.require(:post).permit(:title, :content))
+      redirect_to @post, notice: "Post was successfully updated."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path, notice: "Post was successfully deleted."
+  end
 end
