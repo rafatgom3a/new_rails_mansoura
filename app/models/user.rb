26190f11cbff052e9a1892e33f2c47e155af96f1
rafatgomaa <rafatgomaa815@gmail.com>
2025-05-11 12:25:27 +0300

    Adding validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  validates :name, :dob, :email, :phone_number, presence: true
+
+  VALID_EMAIL_REGEX = /\A[^@\s]+@[^@\s]+\.[^@\s]+\z/
+  validates :email, format: { with: VALID_EMAIL_REGEX }
+  validates :email, uniqueness: true
 end
